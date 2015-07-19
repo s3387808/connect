b08a5852619b18dee0578a0250ddca123cd1fe54
Ubuntu <ubuntu@ip-172-31-8-130.ap-southeast-2.compute.internal>
2015-07-19 11:19:03 +0000

    19-July

--- search.php
+++ search.php
@@ -1,27 +1,83 @@
+<?php 
+
+include ('answer.php');
+if(isset($_SESSION['winename'])){
+header("location: results.php");}
+
+?>
+
 <html>
 <head>
 <title> Sample Page </title>
 </head>
 <body>
-<?php if ( !isset($_GET['name']) ): ?>
-<script>
 
-alert("Database Username required");
-</script>
+<form action="answer.php" method="GET">
+Enter a Winename or a partial name of a wine: <input type="text" name="winename" /><br>
+Enter a Winery name or a partial name of a winery: <input type="text" name="winery" /><br>
 
-<form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-Please enter your Username: <input type="text" name="name" />
-<input type="submit" value="GO" />
+Select a Wine region
+<select value="tableregion"name= region>	
+<option value="" > </option>
+    <?php if (!isset($_GET['region']))
+	{
+	require_once('db.php');
+	//Connect to DB
+	$conn = mysql_connect (DB_HOST, DB_USER, DB_PW);
+	mysql_select_db(DB_NAME, $conn);	
+	$sql="SELECT region_name FROM region; ";
+	$result_region = mysql_query($sql, $conn);	
+	
+	if (isset($_POST['region']))
+	{
+			$region = $_GET['region'];	
+	 		echo "<option value='$region'> $tableregion </option> ";	
+	}
+	elseif (!isset($_POST['region']))
+	{
+		while($row = mysql_fetch_row($result_region))
+		{
+	 		$tableregion = $row[0];	
+	 		echo "<option value='$tableregion'> $tableregion </option> ";	
+		}}}
+	?>
+</select><br>
+Select a Wine variety
+<select value="tablevariety"  name=variety >	
+<option value=""> </option>
+    <?php if (!isset($_GET['variety']))
+	{
+	require_once('db.php');
+	//Connect to DB
+	$conn = mysql_connect (DB_HOST, DB_USER, DB_PW);
+	mysql_select_db(DB_NAME, $conn);	
+	$sql="SELECT variety FROM grape_variety; ";
+	$result_variety = mysql_query($sql, $conn);	
+	
+	if (isset($_POST['variety']))
+	{
+			$region = $_GET['variety'];	
+	 		echo "<option value='$variety'> $tablevariety </option> ";	
+	}
+	elseif (!isset($_POST['variety']))
+	{
+		while($row = mysql_fetch_row($result_variety))
+		{
+	 		$tablevariety = $row[0];	
+	 		echo "<option value='$tablevariety'> $tablevariety </option> ";	
+		}}}
+	?>
+</select>
+
+<br>
+
+<input type="submit"value="GO" />
 </form>
 
-<?php else: ?>
-<p>Your Username: <?=$_GET['name']?></p>
 
-<form action="answer.php?winename=<?=urlencode($_GET['winename'])?>" method="get">
-Please enter a Winename or a partial Winename: <input type="text" name="winename" />
-<input type="submit" value="GO" />
-</form>
->
-<?php endif; ?>
+
+<span><?php echo $error; ?></span>
+
+
 </body>
 </html>
